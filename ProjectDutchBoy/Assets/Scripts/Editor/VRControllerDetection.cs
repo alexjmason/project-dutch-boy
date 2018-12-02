4f40c5da5e1af047f27f7b0ccb4d4eea25ac70cd
Alexander-Mason <alex.j.mason@gmail.com>
2018-12-02 12:14:40 -0800

    Creating some scene and script data for testing

---
+++ ProjectDutchBoy/Assets/Scripts/Editor/VRControllerDetection.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VRControllerDetection : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
